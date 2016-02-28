e66e7c7dc3cd3e112f8162da787ffbd93237961b
Inderpal Singh Aulakh <inderpal58@gmail.com>
2016-02-28 13:07:19 -0800

    Added geo location api to find distance and travel duration and find vehicle layout and calculations

--- app/src/main/java/com/mss/group3/smartshare/controller/FindVehicleController.java
+++ app/src/main/java/com/mss/group3/smartshare/controller/FindVehicleController.java
@@ -3,75 +3,125 @@ package com.mss.group3.smartshare.controller;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.graphics.Point;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.text.format.Time;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.mss.group3.smartshare.R;
+import com.mss.group3.smartshare.model.FindVehicle;
+import com.mss.group3.smartshare.utility.DistanceAndTimeApiCall;
+import com.mss.group3.smartshare.utility.LocationServices;
+import java.io.IOException;
+import java.sql.Date;
 
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by inder on 2016-02-20.
+ * This controller will trigger the find vehicle layout
+ * The purpose is to validate information and find matching vehicle as per schedule
  */
 public class FindVehicleController extends Activity {
 
-
     TimePickerDialog timePickerDialog;
     DatePickerDialog datePickerDialog;
-    TextView get_date,get_start_time,get_end_time;
-    String vehicle_type;
-    int vehicle_capacity;
-    int vehicle_share_range;
-
     Calendar calendar = Calendar.getInstance();
+    FindVehicle findVehicle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.findvehicle);
+        findVehicle = new FindVehicle();
     }
 
-
+    //set date and time for to and from button clicks
     public void setDateTime(View v) {
 
         calendar = Calendar.getInstance();
 
-        switch (v.getId()){
-            case R.id.button_setStartTime:{
+
+
+
+
+        switch (v.getId()) {
+            case R.id.setStartTimeButton: {
 
                 timePickerDialog = new TimePickerDialog(FindVehicleController.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        get_start_time.setText(hourOfDay + ":"+minute);
+                        ((TextView) findViewById(R.id.getStartTime)).setText(hourOfDay + ":" + minute);
+                        Calendar C = Calendar.getInstance();
+                        C.set(0,0,0,hourOfDay,minute);
+
+                        findVehicle.setArrivalTime(C);
+
                     }
-                },calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), android.text.format.DateFormat.is24HourFormat(FindVehicleController.this));
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), android.text.format.DateFormat.is24HourFormat(FindVehicleController.this));
 
                 timePickerDialog.show();
                 break;
             }
-            case R.id.button_setEndTime:{
+            case R.id.setEndTimeButton: {
 
                 timePickerDialog = new TimePickerDialog(FindVehicleController.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        get_end_time.setText(hourOfDay + ":"+minute);
+                        ((TextView) findViewById(R.id.getEndTime)).setText(hourOfDay + ":" + minute);
+                        Calendar C = Calendar.getInstance();
+                        C.set(0,0,0,hourOfDay,minute);
+
+                        findVehicle.setDepartureTime(C);
+
                     }
-                },calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), android.text.format.DateFormat.is24HourFormat(FindVehicleController.this));
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), android.text.format.DateFormat.is24HourFormat(FindVehicleController.this));
 
                 timePickerDialog.show();
                 break;
             }
 
-            case R.id.button_setDate:{
+            case R.id.getToDateButton: {
 
                 datePickerDialog = new DatePickerDialog(FindVehicleController.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                        String dateString = (monthOfYear+1) +"/" + dayOfMonth + "/" + year;
-                        get_date.setText(dateString);
+                        String dateString = (monthOfYear + 1) + "/" + dayOfMonth + "/" + year;
+                        ((TextView) findViewById(R.id.getToDate)).setText(dateString);
+
+                        Calendar C = Calendar.getInstance();
+                        calendar.set(year, monthOfYear, dayOfMonth);
+
+                        findVehicle.setArrivalDate(calendar.getTime());
+                    }
+                }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+
+                datePickerDialog.show();
+                break;
+            }
+
+            case R.id.getFromDateButton: {
+
+                datePickerDialog = new DatePickerDialog(FindVehicleController.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String dateString = (monthOfYear + 1) + "/" + dayOfMonth + "/" + year;
+                        ((TextView) findViewById(R.id.getFromDate)).setText(dateString);
+
+                        Calendar C = Calendar.getInstance();
+                        calendar.set(year, monthOfYear, dayOfMonth);
+
+                        findVehicle.setDepartureDate(calendar.getTime());
+
                     }
                 }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
 
@@ -81,5 +131,67 @@ public class FindVehicleController extends Activity {
         }
     }
 
+    //refresh current location
+    public void getCurrentLocationButtonClick(View v) {
+        String currentAddress = null;
+        getLocation(currentAddress);
+    }
+
+    //find list of vehicles
+    public void processFindVehicleButtonClick(View v) {
+
+        findVehicle.setDepartureAddressLineOneText(((EditText) findViewById(R.id.departureAddressLineOneText)).getText().toString());
+        findVehicle.setDepartureAddressCityNameText(((EditText) findViewById(R.id.departureAddressCityNameText)).getText().toString());
+        findVehicle.setDepartureAddressCountryNameText(((EditText) findViewById(R.id.departureAddressCountryNameText)).getText().toString());
+        findVehicle.setDepartureAddressPostalCodeText(((EditText) findViewById(R.id.departureAddressPostalCodeText)).getText().toString());
+
+        findVehicle.setArrivalAddressLineOneText(((EditText) findViewById(R.id.arrivalAddressLineOneText)).getText().toString());
+        findVehicle.setArrivalAddressCityNameText(((EditText) findViewById(R.id.arrivalAddressCityNameText)).getText().toString());
+        findVehicle.setArrivalAddressCountryNameText(((EditText) findViewById(R.id.arrivalAddressCountryNameText)).getText().toString());
+        findVehicle.setArrivalAddressPostalCodeText(((EditText) findViewById(R.id.arrivalAddressPostalCodeText)).getText().toString());
+
+
+
+        Geocoder geoCoder = new Geocoder(this, Locale.getDefault());
+        findVehicle.findDistanceAndDuration(geoCoder);
+
+
+    }
+
+    //get current location
+    private void getLocation(String address) {
+
+        LocationServices mLocationServices = new LocationServices(this);
+        mLocationServices.getLocation();
+
+        if (mLocationServices.isLocationAvailable == false) {
+
+            //try again
+            Toast.makeText(getApplicationContext(), "Your location is not available, " +
+                    "Enter address manually or Press refresh after enabling location.", Toast.LENGTH_SHORT).show();
+            return;
+
+        } else {
+
+            // Getting location co-ordinates
+            double latitude = mLocationServices.getLatitude();
+            double longitude = mLocationServices.getLongitude();
+            address = mLocationServices.getLocationAddress();
+
+            try {
+
+                ((EditText) findViewById(R.id.departureAddressLineOneText)).setText(mLocationServices.getLineOneAddress());
+                ((EditText) findViewById(R.id.departureAddressCityNameText)).setText(mLocationServices.getLocationCity());
+                ((EditText) findViewById(R.id.departureAddressCountryNameText)).setText(mLocationServices.getLocationCountry());
+                ((EditText) findViewById(R.id.departureAddressPostalCodeText)).setText(mLocationServices.getPostalCode());
+
+            } catch (Exception e) {
+
+            }
+        }
+
+        //close the gps
+        mLocationServices.closeGPS();
+    }
 
 }
---
+++ app/src/main/java/com/mss/group3/smartshare/utility/DistanceAndTimeApiCall.java
@@ -0,0 +1,202 @@
+package com.mss.group3.smartshare.utility;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+
+/**
+ * Created by inder on 2016-02-28.
+ * This class will find the driving distance and duration by geo points
+ */
+public class DistanceAndTimeApiCall {
+
+    //private constructor
+    DistanceAndTimeApiCall() {
+    }
+
+    double lattitudeOne;
+    double lattitudeTwo;
+    double longitudeOne;
+    double longitudeTwo;
+    //in metric unit
+    double distance;
+    double duration;
+
+
+    //public constructor
+    public DistanceAndTimeApiCall(double lat1, double lon1, double lat2, double lon2) {
+        lattitudeOne = lat1;
+        lattitudeTwo = lat2;
+        longitudeOne = lon1;
+        longitudeTwo = lon2;
+        distance = 0;
+        duration = 0;
+    }
+
+    /**
+     * execute thread to find distance and duration
+     */
+    public void calculate()
+    {
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    getDistance(lattitudeOne,longitudeOne, lattitudeTwo, longitudeTwo);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    public double getDistance()
+    {
+        return distance;
+    }
+
+    public double getDuration()
+    {
+        return duration;
+    }
+
+
+    private void getDistance(double lat1, double lon1, double lat2, double lon2) {
+        String result_in_kms = "";
+        String url = "http://maps.google.com/maps/api/directions/xml?origin=" + lat1 + "," + lon1 + "&destination=" + lat2 + "," + lon2 + "&sensor=false&units=metric";
+        String tag[] = {"duration", "distance"};
+        HttpResponse response = null;
+        try {
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpContext localContext = new BasicHttpContext();
+            HttpPost httpPost = new HttpPost(url);
+            response = httpClient.execute(httpPost, localContext);
+            InputStream is = response.getEntity().getContent();
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document doc = builder.parse(is);
+            if (doc != null) {
+                NodeList nl;
+                ArrayList args = new ArrayList();
+                for (String s : tag) {
+                    nl = doc.getElementsByTagName(s);
+                    if (nl.getLength() > 0) {
+                        Node node = nl.item(nl.getLength() - 1);
+                        args.add(node.getTextContent());
+                    } else {
+                        args.add(" - ");
+                    }
+                }
+
+                String timeFind[] = String.valueOf(args.get(0)).split("\\r?\\n");
+                String distanceFind[] = String.valueOf(args.get(1)).split("\\r?\\n");
+
+                for (String s:timeFind) {
+                    if(s.contains("hour") || s.contains("min"))
+                    {
+
+                        String[] splited = s.split(" ");
+                        if(splited.length > 0)
+                        {
+                            int length = splited.length;
+
+                            boolean found = false;
+                            if(length >= 4)
+                            {
+                                if(splited[length-1].contains("min"))
+                                {
+                                    duration = Double.parseDouble(splited[length-2]);
+                                }
+
+                                if(splited[length -3 ].contains("hour"))
+                                {
+                                    duration += Double.parseDouble(splited[length -4])*60;
+                                }
+
+                                found = true;
+                            }
+                            if(!found)
+                            {
+                                if(splited[length-1].contains("min"))
+                                {
+                                    duration = Double.parseDouble(splited[length-2]);
+                                }
+
+                                if(splited[length -1 ].contains("hour"))
+                                {
+                                    duration = Double.parseDouble(splited[length - 2])*60;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+
+                for (String s:distanceFind) {
+                    if(s.contains("Km") || s.contains("m"))
+                    {
+
+                        String[] splited = s.split(" ");
+                        if(splited.length > 0)
+                        {
+                            int length = splited.length;
+
+                            boolean found = false;
+                            if(length >= 4)
+                            {
+                                if(splited[length-1].contains("m"))
+                                {
+                                    distance = Double.parseDouble(splited[length-2]);
+                                }
+
+                                if(splited[length -3 ].contains("km"))
+                                {
+                                    distance += Double.parseDouble(splited[length -4])*1000;
+                                }
+
+                                found = true;
+                            }
+                            if(!found)
+                            {
+                                if(splited[length-1].contains("m"))
+                                {
+                                    distance = Double.parseDouble(splited[length-2]);
+                                }
+
+                                if(splited[length -1 ].contains("km"))
+                                {
+                                    distance = Double.parseDouble(splited[length - 2])*1000;
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+
+
+                result_in_kms = String.valueOf(args.get(0));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Float f = Float.valueOf(result_in_kms);
+
+    }
+
+}
